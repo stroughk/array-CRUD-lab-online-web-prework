0bb9ba90dd625f520c16658f4d2978c215452682
Kateryna Strough <katerynastrough@hotmail.com>
2019-04-21 16:09:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,30 +6,30 @@ def create_an_array
   puts create_an_array["one","two","three","four"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array["one","two","three","four"]
   puts add_element_to_end_of_array.push("five") 
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array["one","two","three","four"]
   puts add_element_to_start_of_array.unshift("zero")
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array["one","two","three","four"]
   puts remove_element_from_end_of_array.pop("one")
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array["one","two","three","four"]
   puts remove_element_from_start_of_array.shift
 end
 
-def retrieve_element_from_index(array, index_number)
+def retrieve_element_from_index["one","two","three","four"]
   puts retrieve_element_from_index[2]
 end
 
-def retrieve_first_element_from_array(array)
+def retrieve_first_element_from_array["one","two","three","four"]
   puts retrieve_first_element_from_array[0] 
 end
 
-def retrieve_last_element_from_array(array)
+def retrieve_last_element_from_array["one","two","three","four"]
   puts retrieve_last_element_from_array[-1]
 end
